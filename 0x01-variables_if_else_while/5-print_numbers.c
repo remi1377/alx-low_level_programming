dc46b063d695d12bfb5b7b5199af815b16b7a04e
remi1377 <remiabuzeid0076@gmail.com>
2023-04-24 17:16:53 -0700

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints numbers from 0 to 9
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	num []= "123456789"
+	for (i = 0 ; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
