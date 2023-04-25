2f72f794f4dd286136cab4a0ff4957ad8d41f3cd
remi1377 <remiabuzeid0076@gmail.com>
2023-04-24 17:22:31 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints numbers from 1 to 9
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	char num[9] = "123456789"
+	for (i = 0; i < 9, i++)
+	{
+		putchar([i]);
+	}
+	putchar('\n')
+	return (0);
+}
