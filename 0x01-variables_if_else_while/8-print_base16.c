a34a234438a8d0237099ec81715e8d21fda85ec3
remi1377 <remiabuzeid0076@gmail.com>
2023-04-24 17:57:36 -0700

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints hexadecimal digits
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char i;
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
