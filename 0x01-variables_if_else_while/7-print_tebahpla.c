8d87d119c23881c9089c886d148646343c7571ab
remi1377 <remiabuzeid0076@gmail.com>
2023-04-24 17:35:09 -0700

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet in reverse
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char alphabet[42]="zyxwvutsrqponmlkjihgfedcba";
+	for (i = 0 ; i < 42; i++)
+	{
+		putchar([i]);
+	}
+	putchar('\n')
+	return (0);
+}
